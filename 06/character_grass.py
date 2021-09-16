60e422c90a451efc8e57b31f2bf5ca1c8720468c
mellow3607 <mellow3607@naver.com>
2021-09-16 23:56:39 +0900

    drill06

--- 06/character_grass.py
+++ 06/character_grass.py
@@ -10,40 +10,50 @@ character = load_image('character.png')
 
 x = 0
 y = 0
+degree = 1
+radian = 0
 while(True):
+    while x < 800:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        x += 2
+        #delay(0.01)
 
-    if x==0 and y == 0 :
-        while x < 800:
-            clear_canvas_now()
-            grass.draw_now(400,30)
-            character.draw_now(x,90)
-            x += 2
-            delay(0.01)
+   
+    while y < 600 :
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(800,y)
+        y += 2
+        #delay(0.01)
 
-    if x == 800 and y == 0 :
-        while y < 600 :
-            clear_canvas_now()
-            grass.draw_now(400,30)
-            character.draw_now(800,y)
-            y += 2
-            delay(0.01)
+    
+    while 0 < x:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,600)
+        x -= 2
+        #delay(0.01)
 
-    if x == 800 and y == 600:
-        while 0 < x:
-            clear_canvas_now()
-            grass.draw_now(400,30)
-            character.draw_now(x,600)
-            x -= 2
-            delay(0.01)
+    
+    while 0 < y:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(0,y)
+        y -= 2
+        #delay(0.01)
 
-    if x == 0 and y == 600:
-        while 0 < y:
-            clear_canvas_now()
-            grass.draw_now(400,30)
-            character.draw_now(0,y)
-            y -= 2
-            delay(0.01)
+    while degree < 360+1:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        radian = degree * pi / 180
+        character.draw_now(360 + cos(radian) * 270 ,360 + sin(radian) * 270)
+        degree += 1
 
-delay(5)
+    if degree == 361 :
+        degree = 0
+
+#delay(5)
 
 close_canvas()
